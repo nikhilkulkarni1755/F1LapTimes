d2b2a6e05ce5627eaf2339c48ed72a088beb590e
Nikhil Kulkarni <nikhilkulkarni1755@gmail.com>
2023-07-03 01:14:42 -0700

    creating all the data. done w creating all the urls

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # F1LapTimes
-Testing out the hypothesis: Does lap time decrease as time goes on? (AKA compare lap times as laps go on). We expect it to decrease, given fuel load decreases. Created with Sameer Kulkarni and Shrikrishna Kulkarni
+Created with Sameer Kulkarni and Shrikrishna Kulkarni
+
+Testing out the hypothesis: Does lap time decrease as time goes on? (AKA compare lap times as laps go on). We expect it to decrease, given fuel load decreases. 
+
+Ways to Test this Hypothesis
+1 - Get the average lap time for each lap. This can be compared with other lap times, we can easily graph these changes
+2 - Don't take the average, compare the driver stats along with the lap times for more comprehensive look
+
+1 is probably the first step.
+
+
--- index.py
+++ index.py
@@ -17,17 +17,48 @@ print("Does lap time decrease as the race goes on?")
 #y axis lap time
 
 url = "https://ergast.com/api/f1/2023/last/laps/1"
-response = requests.get(url).text
+base_url = "https://ergast.com/api/f1/2023/"
 
-soup = BeautifulSoup(response, 'xml')
+#Austrian Grand Prix is Round 9
+#race integer variable
+#https://ergast.com/api/f1/2023/9/laps/1
+race = 1
+laps = "/laps/"
+#lapNumber = 1
+#response = requests.get(url).text
 
-times = soup.find_all('Timing')
+urls = []
+counter = 0
+for offset in range(1, 71):
+    urls.append(base_url + str(race) + laps + str(offset))
+    print(urls[counter])
+    counter+=1
+
+for url in urls:
+    response = requests.get(url).text
+    soup = BeautifulSoup(response, 'xml')
+    times = soup.find_all('Timing')
+    for data in times:
+        #print(data)
+        #print(data['driverId'])
+        lap = data['lap']
+        #print(data['time'])
+        f = open("data/" + str(lap) + "race" + str(race) + ".txt", "a")
+        print(data['time'])
+        f.write(data['time'])
+        f.close()
+        #print(data['position'])
+        #print(data['lap'])
+        #print(data['lap'])
+
 #print(len(times))
 
-for data in times:
+#for url in urls:
+
+#for data in times:
     #print(data)
     #print(data['driverId'])
-    print(data['time'])
+#    print(data['time'])
     #print(data['position'])
     #print(data['lap'])
     #print(data['lap'])
