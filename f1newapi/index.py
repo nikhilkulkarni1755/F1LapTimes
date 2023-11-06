0b444a2b60b21ce16da6b34134b1080ecdd15bad
Nikhil Kulkarni <nikhilkulkarni1755@gmail.com>
2023-11-06 09:51:22 -0800

    [FastF1] found a way to calculate pit stop times

--- f1newapi/index.py
+++ f1newapi/index.py
@@ -1,8 +1,35 @@
 import fastf1
+import pandas as pd
 
 # session = fastf1.get_session(2023, 'Austin', 'Q')
 session = fastf1.get_session(2023, 'Austin', 'R')
+# print(session)
+session.load(telemetry=False, laps=True, weather=True)
 print(session)
-session.load(telemetry=False, laps=False, weather=False)
-hamilton = session.get_driver('HAM')
-print(hamilton['FirstName'])
+
+verOutTime = session.laps.pick_driver('VER').PitOutTime
+verInTime = session.laps.pick_driver('VER').PitInTime
+
+outTimes = []
+for i in verOutTime:
+    if i is not pd.NaT:
+        # print(i)
+        outTimes.append(i)
+
+inTimes = []
+# print('printing out when vers came in to the pits next')
+for i in verInTime:
+    if i is not pd.NaT:
+        # print(i)
+        inTimes.append(i)
+
+
+# print('printing out differences, let us see what happens')
+for i in range(len(outTimes)):
+    print(outTimes[i] - inTimes[i])
+
+# print(verOutTime)
+# print('printing out when vers came in to the pits next')
+# print(verInTime)
+# hamilton = session.get_driver('HAM')
+# print(hamilton['FirstName'])
